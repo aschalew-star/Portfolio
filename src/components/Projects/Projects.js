2e19b93e10e08f37f62fb7eaafa2b2b00001abbc
unknown <aschalewmuleta4@gmail.com>
2025-06-25 09:16:56 -0700

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import emotion from "../../Assets/Projects/emotion.png";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are the one projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              // imgPath={chatify}
+              isBlog={false}
+              title="MEP"
+              description="Merchant exchange platform for ethiopian traditional cloths for sell merchants on platform by register, components realtime chat ,payment in chapa,manage all using MERN crud ."
+              ghLink="https://github.com/aschalew-star/MEP-defence"
+              demoLink="#"
+            />
+          </Col>
+
+      
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={""}
+            // target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            {/* &nbsp; */}
+            Download CV
+          </Button>
+        </Row>
+
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
