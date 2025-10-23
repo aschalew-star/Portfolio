250dd5007fd0248c26ebbd7ad97efc0aab2e5ea4
aschalew-star <aschalewmuleta4@gmail.com>
2025-10-23 01:59:17 -0700

    Update project details for Tender management platform

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,18 +31,18 @@ function Projects() {
             <ProjectCard
               // imgPath={chatify}
               isBlog={false}
-              title="MEP"
-              description="Merchant exchange platform for ethiopian traditional cloths for sell merchants on platform by register, components realtime chat ,payment in chapa,manage all using MERN crud ."
-              ghLink="https://github.com/aschalew-star/MEP-defence"
+              title="Tender frontend"
+              description="A comprehensive, enterprise-ready tender management platform designed to streamline the entire tender lifecycle for organizations and suppliers. The platform allows users to create, manage, and participate in tenders efficiently."
+              ghLink="https://github.com/aschalew-star/Tender"
               demoLink="#"
             />
           </Col> <Col md={4} className="project-card">
             <ProjectCard
               // imgPath={chatify}
               isBlog={false}
-              title="MEP"
-              description="Merchant exchange platform for ethiopian traditional cloths for sell merchants on platform by register, components realtime chat ,payment in chapa,manage all using MERN crud ."
-              ghLink="https://github.com/aschalew-star/MEP-defence"
+              title="Tender Backend"
+              description="A comprehensive, enterprise-ready tender management platform designed to streamline the entire tender lifecycle for organizations and suppliers. The platform allows users to create, manage, and participate in tenders efficiently"
+              ghLink="https://github.com/aschalew-star/Backend-Tender"
               demoLink="#"
             />
           </Col>
